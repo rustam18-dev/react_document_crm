729425393618a1f01edfd3cce02d9743c17cf3f1
rustam18-dev <rustam.radzabov8410@gmail.com>
2024-08-23 17:42:33 +0500

    dataTable

--- src/components/data/DocumentLayoutGrid.tsx
+++ src/components/data/DocumentLayoutGrid.tsx
@@ -1,5 +1,4 @@
 import * as React from "react"
-import styles from "./DocumentLayoutGrid.module.scss"
 import {
   Grid,
   GridColumn as Column,
@@ -15,13 +14,19 @@ import {
 import { setExpandedState, setGroupIds } from "@progress/kendo-react-data-tools"
 
 import products from "./shared-gd-products.json"
-import { Product } from "./shared-gd-interfaces"
+import { IFile } from "./shared-gd-interfaces"
+import {
+  GridCustomCellProps,
+  GridHeaderSelectionChangeEvent,
+  GridSelectionChangeEvent,
+} from "@progress/kendo-react-all"
 
-const initialGroup: GroupDescriptor[] = [{ field: "ProductName" }]
+const DATA_ITEM_KEY = "id"
+const SELECTED_FIELD = "selected"
+const initialGroup: GroupDescriptor[] = [{ field: "is_recent_file" }]
 
-const processWithGroups = (data: Product[], group: GroupDescriptor[]) => {
+const processWithGroups = (data: IFile[], group: GroupDescriptor[]) => {
   const newDataState = groupBy(data, group)
-
   setGroupIds({ data: newDataState, group: group })
 
   return newDataState
@@ -29,9 +34,9 @@ const processWithGroups = (data: Product[], group: GroupDescriptor[]) => {
 
 export const DocumentLayoutGrid = () => {
   const [group, setGroup] = React.useState(initialGroup)
-  const [resultState, setResultState] = React.useState<
-    Product[] | GroupResult[]
-  >(processWithGroups(products, initialGroup))
+  const [resultState, setResultState] = React.useState<IFile[] | GroupResult[]>(
+    processWithGroups(products, initialGroup),
+  )
   const [collapsedState, setCollapsedState] = React.useState<string[]>([])
 
   const onGroupChange = React.useCallback((event: GridGroupChangeEvent) => {
@@ -43,7 +48,6 @@ export const DocumentLayoutGrid = () => {
 
     if (areNewGroupsUnique) {
       const newDataState = processWithGroups(products, event.group)
-
       setGroup(event.group)
       setResultState(newDataState)
     }
@@ -68,24 +72,125 @@ export const DocumentLayoutGrid = () => {
     collapsedIds: collapsedState,
   })
 
+  const onSelectionChange = (e: GridSelectionChangeEvent) => {
+    const newData = resultState.map((states) => {
+      states.items.forEach((item: IFile) => {
+        if (item.id === e.dataItem.id) {
+          if (!item.selected) {
+            item.selected = true
+          } else {
+            delete item.selected
+          }
+        }
+      })
+
+      return states
+    })
+
+    // @ts-ignore
+    setResultState(newData)
+  }
+
+  const onHeaderSelectionChange = React.useCallback(
+    (event: GridHeaderSelectionChangeEvent) => {
+      const checkboxElement: boolean = event.syntheticEvent.target.checked
+
+      let result: (IFile | GroupResult)[] = []
+
+      if (checkboxElement) {
+        result = resultState.map((states) => {
+          states.items.forEach((item: IFile) => {
+            item.selected = true
+          })
+
+          return states
+        })
+      } else {
+        result = resultState.map((states) => {
+          states.items.forEach((item: IFile) => {
+            item.selected = false
+          })
+
+          return states
+        })
+      }
+
+      // @ts-ignore
+      setResultState(result)
+    },
+    [],
+  )
+
   return (
     <Grid
-      className={styles.box_shadow}
+      className={"document_layout"}
       style={{ height: "520px" }}
       data={newData}
+      sortable={true}
       onGroupChange={onGroupChange}
       group={group}
       onExpandChange={onExpandChange}
+      onSelectionChange={onSelectionChange}
       expandField="expanded"
+      dataItemKey={DATA_ITEM_KEY}
+      selectedField={SELECTED_FIELD}
+      onHeaderSelectionChange={onHeaderSelectionChange}
     >
-      <Column field="ProductID" filterable={false} title="ID" width="50px" />
-      <Column field="ProductName" title="Наименование файла / каталога" />
-      <Column field="UnitPrice" title="Дата загрузки" filter="numeric" />
-      <Column field="UnitsInStock" title="Тип документа" filter="numeric" />
-      <Column field="Category.CategoryName" title="Статус" />
-      <Column field="Category.CategoryName" title="Филиал" />
-      <Column field="Category.CategoryName" title="Офис" />
-      <Column field="Category.CategoryName" title="Кто загрузил / изменил" />
+      <Column filterable={false} field={SELECTED_FIELD} width={50} />
+      <Column
+        field="filename"
+        title="Наименование файла / каталога"
+        columnMenu={ColumnMenu}
+        width={280}
+        cells={{
+          data: FileNameCell,
+        }}
+      />
+      <Column
+        field="date_download"
+        columnMenu={ColumnMenu}
+        title="Дата загрузки"
+        filter="numeric"
+        width={150}
+      />
+      <Column
+        field="type_document"
+        columnMenu={ColumnMenu}
+        title="Тип документа"
+        filter="numeric"
+        width={170}
+      />
+      <Column
+        field="status"
+        columnMenu={ColumnMenu}
+        title="Статус"
+        width={170}
+      />
+      <Column
+        field="branch"
+        columnMenu={ColumnMenu}
+        title="Филиал"
+        width={170}
+      />
+      <Column field="office" columnMenu={ColumnMenu} title="Офис" width={170} />
+      <Column
+        field="who_changed_upload"
+        columnMenu={ColumnMenu}
+        title="Кто загрузил / изменил"
+      />
     </Grid>
+  )
+}
+export const ColumnMenu = () => {
+  return <></>
+}
+
+export const FileNameCell = (props: GridCustomCellProps) => {
+  const { dataItem } = props
+
+  return (
+    <td {...props.tdProps}>
+      {dataItem.ProductID} {dataItem.filename}
+    </td>
   )
 }
